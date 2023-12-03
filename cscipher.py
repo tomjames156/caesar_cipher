e56d65cbd94096f73506c314ab3638739d6e859e
tomisin <tomjames156@gmail.com>
2023-12-03 15:36:00 +0100

    added comments

--- cscipher.py
+++ cscipher.py
@@ -1,22 +1,22 @@
 letters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
+] # all english letters
 
 def encrypt(plain_text=''):
+    # encrypts plainn text to cipher text using the Caesar cipher
     cipher_text = ''
-    if plain_text == '':
-        plain_text = input("Enter some plain text to be converted\n=> ")
-
-    print("Converting to Cipher ...")
+    if plain_text == '': # if no text passed as a parameter 
+        plain_text = input("Enter some plain text to be converted\n=> ") # user enters the plain text
 
     for char in plain_text:
         if char.isalpha():
-            new_char = letters[(letters.index(char.upper()) + 3) % 26]
-            if char.isupper():
+            new_char = letters[(letters.index(char.upper()) + 3) % 26] # get the substituted letter for the char in plain text
+            if char.isupper(): # add to cipher in uppercase if plain text was uppercase
                 cipher_text += new_char.upper()
-            else:
+            else: # add to cipher in lowercase if plain text was lowercase
                 cipher_text += new_char.lower()
         else:
+            # add non alphabetic characters without any subtitution
             cipher_text += char
 
     return cipher_text
